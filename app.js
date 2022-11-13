d35d70fccb80dbdcdd372852cbe3256ae810ff08
Vinicius Figueiredo <viniciusfifer@gmail.com>
2022-11-13 19:41:02 -0300

    movie selection randomizer

--- app.js
+++ app.js
@@ -1,203 +1,91 @@
-/*imports*/
-
-const http = require('http');
-const express = require('express')
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const path = require("path");
+const express = require('express');
+const path = require('path');
+const request = require('request');
 require('dotenv').config()
-const app  = express()
-const server = http.createServer(app);
-const bodyParser = require('body-parser');
 
-// Fetch data
-const apiURL = process.env.API_URL
-app.get('/data', async (req, res) => {
-	const response = await fetch(apiURL)
-	const data = response.json()
-	res.json({data})
+const app = express();
+
+const API_KEY = process.env.API_KEY;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('search');
 })
 
-//Config JSON response
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,'./public')));
-
-//Open Route - Public Route
-
-app.get('/',(req,res) => {
-    res.status(200).json({ msg:'API started' })
-    res.sendFile(path.join(__dirname,'./public/index.html'));
+const port = process.env.PORT || 5040;
+app.listen(port, function(){
+    console.log(`server is listening on port: ${port}`);
 });
 
-// Credentials
-
-const dbUser = process.env.DB_USER
-const dbPassword = process.env.DB_PASS
-
-mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.a4qjq63.mongodb.net/?retryWrites=true&w=majority`).then(() => {
-    server.listen(3000, function(){
-        console.log("server is listening on port: 3000");
-    });
-    console.log('Conectou ao banco!')
-}).catch((err) => console.log(err))
-
-//Private Route
-
-app.get("/user/:id", checkToken, async(req, res) => {
-    const id = req.params.id
-
-    //check if user exists
-
-    const user = await User.findById(id, '-password')
-
-    if(!user) {
-        return res.status(404).json({ msg: "Usuário não encontrado" })
-    }
-
-    res.status(200).json({ user })
-
-})
-
-function checkToken(req,res,next) {
-
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(" ")[1]
-
-    if(!token) {
-        return res.status(401).json({ msg: "Acesso negado!" })
-    }
-
-    try {
-        
-        const secret = process.env.SECRET
-
-        jwt.verify(token, secret)
-
-        next()
-
-    }catch(error) {
-        res.status(400).json({ msg: "Token inválido" })
-    }
-
-}
-
-//Register
-app.post('/auth/register', async(req, res) => {
-    const {username, email, password, confirmpassword} = req.body
-
-    //Validations
-    if(!username) {
-        return res.status(422).json({ msg: 'O nome é obrigatório!' })
-    }
-
-    if(!email) {
-        return res.status(422).json({ msg: 'O email é obrigatório!' })
-    }
-
-    if(!password) {
-        return res.status(422).json({ msg: 'O password é obrigatório!' })
-    }
-
-    if(confirmpassword !== password) {
-
-        res.render('registration', {signupRequest: 'success'});
-    /* return res.status(422).json({ msg: 'As senhas não são idênticas!' })*/
+app.get('/result', (req, res)=>{
+   
+    let set_language = "pt-BR";
+    let media = "tv"
+    var media_id;
+    let genre_id = req.query.search;
+    console.log(genre_id);
     
-    }
+    getRandomId(randomIntFromInterval(1, 100));
 
-    //Check if user exists
 
-    const userExists = await User.findOne({email: email})
+    function getRandomId(page_number) {
 
-    if(userExists) {
-        return res.status(422).json({ msg: 'Email já cadastrado!'})
-    }
+        request(`https://api.themoviedb.org/3/discover/${media}?api_key=${API_KEY}&language=${set_language}&
+    sort_by=popularity.desc&include_adult=false&with_genres=${genre_id}&include_video=false&page=${page_number}&
+    watch_region=BR`, (error, response, body) =>    {
 
-    //Create password
+        if(error){
+            console.log(error);
+        }else{
+            r_page = JSON.parse(body);            
+            const random_obj_id = randomIntFromInterval(1, 19);
+            media_id = Object(r_page.results[random_obj_id].id);
 
-    const salt = await bcrypt.genSalt(12)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    //Create user
-
-    const user = new User ({
-        username,
-        email,
-        password: passwordHash,
-    })
-
-    try {
-        await user.save()
-
-        /*res.status(201).json({msg: 'Usuário criado com sucesso!'})*/
-        res.send("<div align ='center'><h2>Registration successful</h2></div><br><br><div align='center'><a href='./login.html'>login</a></div><br><br><div align='center'><a href='./registration.html'>Register another user</a></div>");
-        
-    } catch(error) {
-        console.log(error)
-        res
-        .status(500)
-        .json({ msg: 'Aconteceu um erro no servidor!'})
-        res.send("<div align ='center'><h2>Email already used</h2></div><br><br><div align='center'><a href='./registration.html'>Register again</a></div>");
-    }
-
- })
-
- //Login user
-
- app.post("/auth/login", async(req, res) => {
-    
-    const {email, password} = req.body
-
-    //validations
-
-    if(!email) {
-        return res.status(422).json({ msg: 'O email é obrigatório!' })
-    }
-
-    if(!password) {
-        return res.status(422).json({ msg: 'O password é obrigatório!' })
-    }
-
-    //Check if users exists
-
-    const user = await User.findOne({email: email})
-
-    if(!user) {
-        return res.status(422).json({msg: 'Usuário não encontrado!'})
-    }
-
-    //Check if password matches
-
-    const checkPassword = await bcrypt.compare(password, user.password)
-
-    if(!checkPassword) {
-        return res.status(422).json({ msg:'Senha inválida!' })
-    }
-
-    try {
-        const secret = process.env.SECRET
-
-        const token  = jwt.sign(
-            {
-            id: user._id
-            },
-            secret,
-        )
-
-        res.status(200).json({ msg: "Autenticação realizada com sucesso!", token})
-        res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${username}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
-
-    } catch (err) {
-        console.log(error)
-
-        res.status(500).json({
-            msg: 'Aconteceu um erro no servidor!'
+            renderRandomPage(media_id);
+            } 
         })
-        res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
     }
 
- })
+    const getProvider = async(media_id)  => {
+        return fetch(`https://api.themoviedb.org/3/${media}/${media_id}/watch/providers?api_key=${API_KEY}`).then((response) => { 
+            return response.json().then((data) => {
+                return data;
+            }).catch((err) => {
+                console.log(err);
+            }) 
+        });
+    }
 
+    const renderRandomPage = async(media_id) => {
+
+        let providers = await getProvider(media_id);
+        let local_providers = providers.results.BR;
+
+        console.log(Object.keys(local_providers));
+        
+        if(local_providers.hasOwnProperty("flatrate")) {
+            console.log(Object.keys(local_providers.flatrate[1].provider_name));
+        }
+
+        request(`https://api.themoviedb.org/3/${media}/${media_id}?api_key=${API_KEY}&language=${set_language}`, (error, response, body)=>{
+            if(error){
+                console.log(error);
+            }else{
+                let data = JSON.parse(body);
+    
+                res.render('result', {data: data, local_providers: local_providers});
+            }  
+        })
+    }
+
+
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+      }
+
+});
 
